b40910301a21c87a4dfd403b40a2e8cd2cb4e782
Elias J. Perez <elias.perez@harrys.com>
2018-04-11 09:43:22 -0400

    fixup! [Params] To hash method not returning correct value

--- src/amber/router/params.cr
+++ src/amber/router/params.cr
@@ -68,6 +68,7 @@ module Amber::Router
       params_hash = Hash(String, String | Array(String) | Nil).new
       query.each { |key, _| params_hash[key] = query[key] }
       route.each { |key, _| params_hash[key] = route[key] }
+      json.each { |key, _| params_hash[key] = json[key] }
       multipart.each { |key, _| params_hash[key] = multipart[key] }
       form.each { |key, _| params_hash[key] = form[key] }
       params_hash
